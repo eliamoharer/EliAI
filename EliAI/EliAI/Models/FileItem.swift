d8662c8839552bd488e356d67c03032127d75606
Elia Moharer <116987343+eliamoharer@users.noreply.github.com>
2026-02-11 18:15:02 -0500

    restart

--- EliAI/EliAI/Models/FileItem.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct FileItem: Identifiable, Hashable {
-    let id = UUID()
-    let name: String
-    let isDirectory: Bool
-    var children: [FileItem]?
-    let path: URL
-}
