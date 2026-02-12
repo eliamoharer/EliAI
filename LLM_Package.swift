f6f45e375a8f271e59aab77663e72334c9102996
Elia Moharer <116987343+eliamoharer@users.noreply.github.com>
2026-02-12 02:20:09 -0500

    umm

---
+++ LLM_Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "LLM",
+    platforms: [.macOS(.v13), .iOS(.v16)],
+    products: [
+        .library(name: "LLM", targets: ["LLM"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.1"),
+    ],
+    targets: [
+        .target(
+            name: "LLM",
+            dependencies: [
+                "llm",
+                "LLMMacrosImplementation",
+            ],
+            path: "Sources/LLM"
+        ),
+        .binaryTarget(
+            name: "llm",
+            path: "llama.cpp/llama.xcframework"
+        ),
+        .macro(
+            name: "LLMMacrosImplementation",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        )
+    ]
+)
